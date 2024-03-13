70c855119d9b192dc0419a6e728d0eea09ae3b0a
dkalariya <deep@conversios.io>
2024-03-13 16:32:46 +0530

    node

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
 
-mongoose.connect(process.env.mongourl,
+mongoose.connect("mongodb://localhost:27017/students-api",
 {
     // useCreateIndex:true,
     useNewUrlParser:true,
